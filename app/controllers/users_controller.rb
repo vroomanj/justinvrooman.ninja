42a09f87d6b6452749c224039e328407d0f1647f
Justin Vrooman <vroomanj@gmail.com>
2015-04-23 09:58:01 -0400

    improve admin crud

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,6 +13,20 @@ class UsersController < ApplicationController
   	@user = User.find(params[:id])
   end
 
+  def create
+    @user = User.new(user_params)
+
+    respond_to do |format|
+      if @user.save
+        format.html { redirect_to users_path, notice: 'User was successfully created.' }
+        format.json { render :index, status: :created, location: @user }
+      else
+        format.html { render :new }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
   def update
     if user_params[:password].blank?
       user_params.delete(:password)
